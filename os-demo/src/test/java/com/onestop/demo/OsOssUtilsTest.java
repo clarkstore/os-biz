0ec23188584404e253eb563a089ff5584c2111d5
Clark <changhongyuan@126.com>
2021-09-17 17:02:20 +0800

    修改：OSS测试工具类

--- os-demo/src/test/java/com/onestop/demo/OsOssUtilsTest.java
+++ os-demo/src/test/java/com/onestop/demo/OsOssUtilsTest.java
@@ -1,6 +1,7 @@
 package com.onestop.demo;
 
 import com.onestop.ali.oss.util.OsOssUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,20 +13,29 @@ import java.io.FileInputStream;
  * @author Clark
  * @version 2021/8/20
  */
+@Slf4j
 @SpringBootTest(classes = OsDemoApplication.class)
 public class OsOssUtilsTest {
+    private String ossPath = "site/operator/";
+    private String localPath = "d:/upload";
+
     @Autowired
     private OsOssUtils utils;
 
     @Test
     public void test() {
-        File file = new File("d:\\123.txt");
-
         try {
-            utils.upload("abc/1.txt", new FileInputStream(file));
-            utils.download("abc/1.txt", "d:\\1.txt");
-            utils.deleteObject("abc");
-            utils.deleteObjectList("abc");
+            //加载文件夹
+            File file = new File(localPath);
+            if (file.isDirectory()) {
+                //将该目录下的所有文件放置在一个File类型的数组中
+                File[] fileList = file.listFiles();
+                log.error("=============================");
+                log.error("本次上传共计：" + fileList.length + "个文件");
+                for (File item : fileList) {
+                    utils.upload(ossPath + item.getName(), new FileInputStream(item));
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
