e2d047a94aa00b4a8572b3792d85e1f09832f672
Clark <changhongyuan@126.com>
2022-10-25 16:41:07 +0800

    修改：优化定时任务

--- biz-task/README.md
+++ biz-task/README.md
@@ -1,4 +1,4 @@
 ### 定时任务Demo模块
-#### 两种方案
-1. TaskApi方案，通过OsTaskSchedulerUtils控制任务调用
-2. BizJob方案，通过@Scheduled注解实现，启动项目访问http://localhost:8080/ssa/#/
+#### 两种方案（推荐方案1）
+1. BizJob方案，通过@Scheduled注解实现，启动项目访问http://localhost:8080/ssa/#/
+2. TaskApi方案，通过OsTaskSchedulerUtils控制任务调用
--- biz-task/src/main/java/com/onestop/biz/task/job/BizJob.java
+++ biz-task/src/main/java/com/onestop/biz/task/job/BizJob.java
@@ -11,8 +11,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class BizJob {
+    /**
+     * 定时任务：业务1
+     */
+    @Scheduled(cron = "0/7 * * * * ? ")
+    public void task1() {
+        log.warn("---------业务1--------");
+    }
+
+    /**
+     * 定时任务：业务2
+     */
     @Scheduled(cron = "0/10 * * * * ? ")
-    public void job() {
-        log.warn("---------BizJob--------");
+    public void task2() {
+        log.warn("---------业务2--------");
     }
 }
